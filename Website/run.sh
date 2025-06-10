95f099d5cf78a2ab51e8cb47b90583ed190bacf4
Ruben van Breda <re.vanbreda@gmail.com>
2025-06-10 19:35:53 +0200

    Added Docker Services

--- Website/run.sh
+++ Website/run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-# This script builds a Docker image for a static website and runs it.
-# Ensure you have Docker installed and running before executing this script.
-# Stop any existing container
-docker stop static-web || true
-docker rm static-web || true    
-docker build -t static-web .
-docker run -d -p 8000:8000 --name static-web static-web
+
+service_name="static-web"
+port=8080
+echo "Building $service_name service"
+echo "Using port $port"
+echo "----------"
+echo "Running $service_name service"
+docker stop $service_name || true
+# Optional: Remove image after run (if you want full cleanup)
+docker rmi $service_name
+docker rm $service_name || true
+docker build -t $service_name .
+
+
+echo "Running Python HTTP server on port 8000"
+docker run -p 8080:8000 $service_name
 # python3 -m http.server 8000 --directory src
+# echo "Service $service_name is running on port $port"

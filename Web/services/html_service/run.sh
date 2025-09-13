c104d5136dc647d0cddcdf35429db3d7545e1d81
Ruben van Breda <re.vanbreda@gmail.com>
2025-09-13 12:10:52 +0200

    Version 0.0.7

--- Web/services/html_service/run.sh
+++ Web/services/html_service/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 service_name="html_service"
-port=5003
+port=3001
 echo "Building $service_name service"
 echo "Using port $port"
 echo "----------"
--- git_repo_update.sh
+++ git_repo_update.sh
@@ -9,6 +9,16 @@ if [ "$discard_changes" == "y" ]; then
 else
     echo "Keeping changes..."
 fi
+
+# Git force pull changes
+echo "Do you want to force pull changes? (y/n)"
+read force_pull_changes
+if [ "$force_pull_changes" == "y" ]; then
+    echo "Force pulling changes..."
+    git fetch origin && git reset --hard origin/main
+else
+    echo "Skipping force pull..."
+
 #prompt user if they want to pull changes
 echo "Do you want to pull changes? (y/n)"
 read pull_changes
